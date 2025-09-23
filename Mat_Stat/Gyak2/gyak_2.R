2423f379e749334e139658af62c390e834b82f6f
Mihalyi Geza <mihgeza2000@gmail.com>
2025-09-23 21:07:02 +0200

    First week of Second Year starting

---
+++ Mat_Stat/Gyak2/gyak_2.R
@@ -0,0 +1,11 @@
+# Első feladat.
+
+# a - Torzítatlan
+# b - Torzítatlan
+# c - Torzított - Keresni kell valamilyen olyan konstant szorzó
+#tényezőt, ami amivel le hete hozni m-re. 
+#Mert az E(X)-nek m-nek kell lennie, de a E(T(X)) ebben az esetben 
+#2/9 m , vagyis kell egy konstant ami 9/2
+# d - Torzítatlan
+
+# Második feladat.
